e8a647b348db76a675b421dc593af0e596a4dd01
Joao Lucas <jldecristo@gmail.com>
2025-06-20 14:25:28 -0300

    Controller com foodList

--- src/main/java/com/fullstack/cardapio/controller/FoodController.java
+++ src/main/java/com/fullstack/cardapio/controller/FoodController.java
@@ -1,16 +1,27 @@
 package com.fullstack.cardapio.controller;
 
 
+import com.fullstack.cardapio.entity.Food;
+import com.fullstack.cardapio.repository.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("food")
 public class FoodController {
 
+    @Autowired
+    private FoodRepository repository;
+
     @GetMapping
-    public void getAll(){
+    public List<Food> getAll(){
+
+        List<Food> foodList = repository.findAll();
+        return foodList;
 
     }
 
