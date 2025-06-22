651aa5e349d4304eb1965a8a5790982dc24daa23
Joao Lucas <jldecristo@gmail.com>
2025-06-21 22:39:32 -0300

    propriedades do constructor Food que recebe o FoodRequestDTO

--- src/main/java/com/fullstack/cardapio/entity/Food.java
+++ src/main/java/com/fullstack/cardapio/entity/Food.java
@@ -23,6 +23,8 @@ public class Food {
     private Integer price;
 
     public Food(FoodRequestDTO data) {
-
+        this.image = data.image();
+        this.price = data.price();
+        this.title = data.title();
     }
 }
