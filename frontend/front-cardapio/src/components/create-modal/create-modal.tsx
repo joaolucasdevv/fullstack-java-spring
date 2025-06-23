cb497df5f8a5948a3971b0bab7fcb5aab3f5c924
Joao Lucas <jldecristo@gmail.com>
2025-06-23 19:59:00 -0300

    estrutura do nosso createmodal

--- frontend/front-cardapio/src/components/create-modal/create-modal.tsx
+++ frontend/front-cardapio/src/components/create-modal/create-modal.tsx
@@ -4,6 +4,13 @@ interface CreateModalProps {
 
 export function CreateModalProps(props : CreateModalProps) {
     return (
-        <></>
+        <div className="modal-overlay">
+            <div className="modal-body">
+                <h2>Cadastre um novo item no Cardápio</h2>
+                <form className="input-container">
+
+                </form>
+            </div>
+        </div>
     )
 }
