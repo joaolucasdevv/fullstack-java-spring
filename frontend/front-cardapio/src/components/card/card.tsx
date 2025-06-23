897218e1c9d751fd1cf29038967683519a05ff8b
Joao Lucas <jldecristo@gmail.com>
2025-06-23 12:50:10 -0300

    card

--- frontend/front-cardapio/src/components/card/card.tsx
+++ frontend/front-cardapio/src/components/card/card.tsx
@@ -1,8 +1,15 @@
 interface CardProps {
-
+    price: number,
+    title: string,
+    image: string
 }
-export function Card(props : CardProps) {
+
+export function Card({ price, image, title } : CardProps){
     return(
-        <></>
+        <div className="card">
+            <img src={image}/>
+            <h2>{title}</h2>
+            <p><b>Valor:</b>{price}</p>
+        </div>
     )
 }
