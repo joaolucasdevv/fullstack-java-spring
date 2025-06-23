429e48201ffc9064340b08c06463ddfdb5a0d54a
Joao Lucas <jldecristo@gmail.com>
2025-06-23 12:55:54 -0300

    data que vai representar os dados vindo da API

--- frontend/front-cardapio/src/App.tsx
+++ frontend/front-cardapio/src/App.tsx
@@ -2,11 +2,14 @@
 import './App.css'
 
 function App() {
+
+  const data = [];
+
   return (
     <div className="container">
       <h1>Cardápio</h1>
       <div className="card-grid">
-        
+
       </div>
     </div>
   )
