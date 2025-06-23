f4fafbe6f2513317751f569bd2fd4245e6207ccc
Joao Lucas <jldecristo@gmail.com>
2025-06-23 20:36:51 -0300

    de query para mutation

--- frontend/front-cardapio/src/hooks/useFoodDataMutate.ts
+++ frontend/front-cardapio/src/hooks/useFoodDataMutate.ts
@@ -1,18 +1,17 @@
 import axios, { type AxiosPromise } from "axios"
 import type { FoodData } from "../interface/FoodData";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 const API_URL = 'http://localhost:8080'
 
-const fetchData = async (): AxiosPromise<FoodData[]> => {
-    const response = axios.get(API_URL + '/food')
+const postData = async (data: FoodData): AxiosPromise<any> => {
+    const response = axios.post(API_URL + '/food', data);
     return response;
 }
 
 export function useFoodData() {
-    const query = useQuery({
-        queryFn: fetchData,
-        queryKey: ['food-data'],
+    const query = useMutation({
+        mutationFn: postData,
         retry: 2
     })
 
